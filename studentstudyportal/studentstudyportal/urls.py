11355c51138fd265fa23718f1519492f5e14543c
Tuffour Boateng <tuffour.boateng@amalitech.com>
2022-06-28 08:03:03 -0700

    heroku deployment config

--- studentstudyportal/studentstudyportal/urls.py
+++ studentstudyportal/studentstudyportal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from studentportal import views as dash_views
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('studentportal.urls')),
@@ -25,4 +27,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="dashboard/login.html"), name='login'),
     path('profile/', dash_views.profile, name='profile' ),
     path('logout/', auth_views.LogoutView.as_view(template_name="dashboard/logout.html"), name='logout'),
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
